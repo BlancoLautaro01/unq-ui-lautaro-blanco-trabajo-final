06d2b8d4b1641dd33a3a6e7bf7bd9fdf7207e23e
Lautaro Blanco <blanco.lautaro@outlook.com>
2021-07-09 13:05:32 -0300

    Componente Dado credao

---
+++ src/dado/Dado.js
@@ -0,0 +1,66 @@
+import React, { useEffect, useState } from "react";
+import dado1 from './images/dado1.jpeg';
+import dado2 from './images/dado2.jpeg';
+import dado3 from './images/dado3.jpeg';
+import dado4 from './images/dado4.jpeg';
+import dado5 from './images/dado5.jpeg';
+import dado6 from './images/dado6.jpeg';
+import './Dado.css';
+
+export function Dado() {
+
+    const number = Math.floor(Math.random() * 6) + 1;
+    const [img, setImg] = useState();
+    const [fixed, setFix] = useState(false);
+
+    useEffect(() => {
+            console.log(number);
+            setImgSwitch();
+        },[]
+    );
+
+    const setImgSwitch = () => {
+
+        switch(number) {
+            case 1:
+                setImg(dado1);
+                break;
+            case 2:
+                setImg(dado2);
+                break;
+            case 3:
+                setImg(dado3);
+                break;
+            case 4:
+                setImg(dado4);
+                break;
+            case 5:
+                setImg(dado5);
+                break;
+            case 6:
+                setImg(dado6);
+                break;
+            default:
+                setImg();
+          }
+    };
+
+    const handleClick = () => {
+        setFix(true);
+    }
+
+    return(
+        <div className="container">
+            <div className="box">
+                <div className="row">
+                    <img className="image" src={img} fluid="true" alt="logo"/>
+                </div>
+                <div className="row">
+                    <button type="button" className={fixed ? "unfixed" : "fixed"} onClick={handleClick}>
+                        {fixed ? "Desfijar" : "Fijar"}
+                    </button>
+                </div>
+            </div>
+        </div>
+    )
+}
