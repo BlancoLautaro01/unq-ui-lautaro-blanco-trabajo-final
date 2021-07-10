4d6294ecbaa79e523ba894d4ff36b78cf7ee3555
Lautaro Blanco <blanco.lautaro@outlook.com>
2021-07-10 18:57:13 -0300

    Top components condidicionales

--- src/Generala.js
+++ src/Generala.js
@@ -4,32 +4,61 @@ import { images, randomNumbers, initializeDados } from "./Randomize";
 import "bootstrap/dist/css/bootstrap.css";
 import "./App.css";
 
+function TopButtom(props) {
+  return (
+    <button type="button" className="clickable" onClick={props.fn}>
+      {props.string}
+    </button>
+  );
+}
+
 export function Generala() {
-  const [turnsLeft, setTurnsLeft] = useState(3);
+  const [turnsLeft, setTurnsLeft] = useState(2);
   const [dados, setDados] = useState(initializeDados());
 
+  const refresh = (isLastRefresh) => {
+    const newNumbers = randomNumbers();
+
+    setDados(
+      dados.map((dado, i) =>
+        dado.isFixed
+          ? {
+              number: dado.number,
+              isFixed: dado.isFixed,
+              img: dado.img,
+            }
+          : {
+              number: newNumbers[i],
+              isFixed: isLastRefresh,
+              img: images[newNumbers[i] - 1],
+            }
+      )
+    );
+  };
+
   const handleRefresh = () => {
-    if (turnsLeft > 0) {
-      const newNumbers = randomNumbers();
-
-      setDados(
-        dados.map((dado, i) =>
-          dado.isFixed
-            ? {
-                number: dado.number,
-                isFixed: dado.isFixed,
-                img: dado.img,
-              }
-            : {
-                number: newNumbers[i],
-                isFixed: false,
-                img: images[newNumbers[i] - 1],
-              }
-        )
-      );
-
+    if (turnsLeft == 2) {
+      refresh(false);
       setTurnsLeft(turnsLeft - 1);
+    } else if (turnsLeft == 1) {
+      refresh(true);
+      setTurnsLeft(turnsLeft - 1);
+      //endTurn()
     }
+  };
+
+  const stay = () => {
+    setTurnsLeft(0);
+
+    setDados(
+      dados.map((dado) => ({
+        number: dado.number,
+        isFixed: true,
+        img: dado.img,
+      }))
+    );
+
+    //endTurn()
   };
 
   const setFix = (i) => {
@@ -50,21 +79,23 @@ export function Generala() {
     );
   };
 
+  const endTurn = () => {};
+
   return (
     <div className="container">
       <div className="row justify-content-md-center">
-        <div className="col col-lg-2"></div>
+        <div className="col col-lg-2">
+          {turnsLeft > 0 ? <TopButtom string="Plantarse" fn={stay} /> : null}
+        </div>
         <div className="col-md-auto">
-          <button
-            type="button"
-            className={turnsLeft > 0 ? "reload" : "dontReload"}
-            onClick={handleRefresh}
-          >
-            {turnsLeft > 0 ? "Volver a tirar" : "Turnos agotados"}
-          </button>
+          {turnsLeft > 0 ? (
+            <TopButtom string="Volver a Tirar" fn={handleRefresh} />
+          ) : null}
         </div>
         <div className="col col-lg-2">
-          <span className="badge badge-light">Turnos: {turnsLeft}</span>
+          <span className="badge badge-light">
+            Tiradas Restantes: {turnsLeft}
+          </span>
         </div>
       </div>
       <div className="row top-buffer">
@@ -79,6 +110,7 @@ export function Generala() {
           </div>
         ))}
       </div>
+      <div className="row top-buffer"></div>
     </div>
   );
 }
