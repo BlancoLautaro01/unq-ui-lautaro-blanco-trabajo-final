90a97e709c1a53edd1ad41ebd69cf9019996ef29
Lautaro Blanco <blanco.lautaro@outlook.com>
2021-07-15 19:32:21 -0300

    Start buttom

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from "react";
 import { Generala } from "./generala/Generala";
+import { Start } from "./start/Start";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <Router>
       <Switch>
-        <Route path="/generala">
-          <Generala />
-        </Route>
+        <Route exact path="/" component={Start} />
+        <Route path="/generala" component={Generala} />
       </Switch>
     </Router>
   );
--- src/start/Start.js
+++ src/start/Start.js
@@ -1,3 +1,22 @@
-import { React } from "react";
-import { Generala } from "./generala/Generala";
+import React from "react";
+import { useHistory } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.css";
+import "./Start.css";
 
+export function Start() {
+  const history = useHistory();
+
+  const handleStart = () => {
+    history.push("/generala");
+  };
+
+  return (
+    <div className="container">
+      <div className="row justify-content-md-center top">
+        <buttom className="start" onClick={handleStart} value="Start">
+          Start
+        </buttom>
+      </div>
+    </div>
+  );
+}
