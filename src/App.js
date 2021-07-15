6baaca8749aaab916adc78215e9d9126a1cd288e
Lautaro Blanco <blanco.lautaro@outlook.com>
2021-07-15 18:43:09 -0300

    Rutas iniciadas

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React from "react";
 import { Generala } from "./generala/Generala";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
-  return <Generala />;
+  return (
+    <Router>
+      <Switch>
+        <Route path="/generala">
+          <Generala />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
